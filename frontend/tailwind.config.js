f87457f1a625ae9d091ea64a2f08e87840659c0b
academy_intern <uyangizwalapho@gmail.com>
2021-04-22 15:05:57 +0200

    Added homepage

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
     },
     darkMode: 'class', // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+        fontFamily:{
+          body:['Nunito']
+        }
+      },
     },
     variants: {
       extend: {},
