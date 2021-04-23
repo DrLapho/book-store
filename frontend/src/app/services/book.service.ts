c70e33f646d1ebf7f102fe290acabd8642ed2ab7
academy_intern <hastings@thedigitalacademy.co.za>
2021-04-23 11:02:22 +0200

    environmental variables

--- frontend/src/app/services/book.service.ts
+++ frontend/src/app/services/book.service.ts
@@ -1,33 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  url = 'http://localhost:4000/books';
+  url = environment.hostUrl;
   constructor(private httpClient: HttpClient) {
     this.getBooks();
   }
-
+//Add a book to the db
   addBook(data) {
     return this.httpClient.post(`${this.url}`, data).subscribe((data) => {
     
     });
   }
-
+//Request to delete a specific book from the db
   deleteBook(id) {
     return this.httpClient.delete(`${this.url}/${id}`);
   }
-
+//Request all books from the db
   getBooks() {
     return this.httpClient.get(`${this.url}`);
   }
-
+//Requesting a specific book from the db
   getBook(id){
     return this.httpClient.get(`${this.url}/${id}`);
   }
-
+  //Sending a request to the db to update a book 
   editBook(data,id){
   return this.httpClient.put(`${this.url}/${id}`,data);
   }
